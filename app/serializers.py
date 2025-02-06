79b1f4228252ae2f2c6e6e88b97bd1a6484925f0
muhammadtaha0022 <muhammadtaha00224443@gmail.com>
2025-02-06 13:11:40 +0500

    lecture 11

--- app/serializers.py
+++ app/serializers.py
@@ -7,3 +7,20 @@ class  EmployeeSerializers(serializers.Serializer):
     email = serializers.EmailField(max_length=254)
     password = serializers.CharField(max_length=50)
     phone = serializers.CharField(max_length=50)
+    
+    def create(selt,validated_data):
+        print('create method fatima ')
+        return Employee.objects.create(**validated_data)
+    
+    def update(self, employee, validated_data):
+        newemployee = Employee(**validated_data)
+        newemployee.id = employee.id
+        newemployee.save()
+        return newemployee
+    
+class  UserSerializers(serializers.Serializer):
+    username = serializers.CharField(max_length=50)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    email = serializers.EmailField(max_length=254)
+    password = serializers.CharField(max_length=50)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,56 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Employee
-from .serializers import EmployeeSerializers
+from .serializers import EmployeeSerializers,UserSerializers
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
+@csrf_exempt
 def employeeListView(request):
-    employee = Employee.objects.all()
-    serializer = EmployeeSerializers(employee,many = True)
-    return JsonResponse(serializer.data,safe=False)
+    if request.method == 'GET':
+        employee = Employee.objects.all()
+        serializer = EmployeeSerializers(employee,many = True)
+        return JsonResponse(serializer.data,safe=False)
+    elif request.method =='POST': 
+        jsondata = JSONParser().parse(request)
+        serializer = EmployeeSerializers(data =jsondata)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, sefe= False)
+        else:
+            return JsonResponse(serializer.errors,safe= False)
+            
+        # return JsonResponse(serializer.data,safe=False)  
+        
+@csrf_exempt
+def EmployeeDetailView(request , pk):
+    try:
+        employee = Employee.objects.get(pk=pk)
+        
+    except Employee.DoesNotExist:
+        return HttpResponse(status= 404)
+    
+     
+    if request.method == 'DELETE':
+        employee.delete()
+        return HttpResponse(status = status.HTTP_204_NO_CONTENT )
+    elif request.method == 'GET':
+        serializer = EmployeeSerializers(employee)
+        return JsonResponse(serializer.data , safe =False)
+        
+    elif request.method == 'PUT':
+        jsondata = JSONParser().parse(request)
+        serializer = EmployeeSerializers(employee, data =jsondata)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, sefe= False)
+        else:
+            return JsonResponse(serializer.errors,safe= False)    
+
+        
+def userListView(request):
+        users = User.objects.all()
+        serializer = UserSerializers(users,many= True)
+        return JsonResponse(serializer.data,safe= False)
