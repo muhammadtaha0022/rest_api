2091fe91610c6312227515b8204d8d055d6724d5
muhammadtaha0022 <muhammadtaha00224443@gmail.com>
2025-02-08 15:51:16 +0500

    lecture 25

--- cbvproject/cbvapp/views.py
+++ cbvproject/cbvapp/views.py
@@ -8,27 +8,46 @@ from django.http import Http404
 from rest_framework import mixins,generics
 
 
-class CourseListView(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
+class CourseListView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializar
-    def get(self,request):
-        return self.list(request)
-    def post(self,request):
-        return self.create(request)
-    
-    
-class CourseDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+
+
+class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializar
+
+
+# class CourseListView(generics.ListAPIView, generics.CreateAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializar
+
+# class CourseDetailView(generics.RetrieveAPIView,generics.UpdateAPIView,generics.DestroyAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializar
+
+
+# class CourseListView(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
+    # queryset = Course.objects.all()
+    # serializer_class = CourseSerializar
+    # def get(self,request):
+    #     return self.list(request)
+    # def post(self,request):
+    #     return self.create(request)
+  
     
-    def get(self,request,pk):
-        return self.retrieve(request,pk)
+# class CourseDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializar
     
-    def put(self,request, pk):
-        return self.update(request,pk)
+#     def get(self,request,pk):
+#         return self.retrieve(request,pk)
     
-    def delete(self ,request, pk):
-        return self.destroy(request,pk) 
+#     def put(self,request, pk):
+#         return self.update(request,pk)
+    
+#     def delete(self ,request, pk):
+#         return self.destroy(request,pk) 
 
 
 
